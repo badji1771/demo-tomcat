fe6c319109fef6a814177a7d6cb5d73a9e6c8a5b
mbadji <mbadji1771@gmail.com>
2024-05-27 15:36:44 +0000

    initial commit

---
+++ src/test/java/org/example/demotomcat/DemoTomcatApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.demotomcat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoTomcatApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
